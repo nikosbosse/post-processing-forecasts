7157ee538a1abac334c1216b49750419bd6b2cb1
Joel Beck <joel.beck@t-online.de>
2021-12-06 20:10:49 +0100

    separated into two helper files

--- R/helpers_preprocessing.R
+++ R/helpers_preprocessing.R
@@ -25,7 +25,7 @@ process_model_input <- function(df, model) {
     # input df is already filtered, take only existing model
     model <- df$model[1]
   }
-
+  
   return(list(df = df, model = model))
 }
 
@@ -45,14 +45,14 @@ filter_quantile_pairs <- function(df, quantiles) {
 process_quantile_pair <- function(df, quantile) {
   lower_quantile <- quantile
   upper_quantile <- 1 - quantile
-
+  
   df |>
     filter_quantile_pairs(quantile) |>
     dplyr::mutate(quantile = factor(.data$quantile) |>
-      forcats::fct_recode(
-        "lower" = as.character(lower_quantile),
-        "upper" = as.character(upper_quantile)
-      ))
+                    forcats::fct_recode(
+                      "lower" = as.character(lower_quantile),
+                      "upper" = as.character(upper_quantile)
+                    ))
 }
 
 add_quantile_group <- function(df, quantiles) {
@@ -114,46 +114,4 @@ change_to_date <- function(df) {
 
 filter_methods <- function(df, methods) {
   df |> dplyr::filter(.data$method %in% methods)
-}
-
-
-
-# TODO: move all above code to own file preprocessing_helpers.R
-# these are needed for functions across many files
-# keep only helpers exclusively used for plotting in this file (as below)
-
-facet_horizon <- function(df, quantile, horizon) {
-  df |>
-    process_quantile_pair(quantile) |> 
-    mutate_horizon() 
-}
-
-facet_quantile <- function(df, quantiles, horizon) {
-  df |> 
-    filter_horizon(horizon) |> 
-    filter_quantile_pairs(quantiles) |> 
-    add_quantile_group(quantiles)
-}
-
-setup_intervals_plot <- function(df) {
-  df |> 
-    tidyr::pivot_wider(names_from = .data$quantile, values_from = .data$prediction) |>
-    ggplot2::ggplot(mapping = ggplot2::aes(x = .data$target_end_date)) +
-    ggplot2::geom_point(ggplot2::aes(y = .data$true_value), size = 1) +
-    ggplot2::geom_line(ggplot2::aes(y = .data$true_value)) +
-    ggplot2::geom_errorbar(
-      ggplot2::aes(ymin = .data$lower, ymax = .data$upper, color = .data$method),
-      position = ggplot2::position_dodge2(padding = 0.01)
-    ) +
-    ggplot2::scale_color_brewer(palette = "Set1") +
-    ggplot2::labs(
-      x = NULL, y = NULL, color = NULL,
-      subtitle = "Prediction methods separated by color"
-    ) +
-    ggplot2::theme_light() +
-    ggplot2::theme(
-      plot.title = ggplot2::element_text(hjust = 0.5),
-      plot.subtitle = ggplot2::element_text(hjust = 0.5),
-      legend.position = "top"
-    )
 }

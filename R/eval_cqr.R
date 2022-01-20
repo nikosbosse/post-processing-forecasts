84c8c23b2223b62d0d29001de2d61e8465af38f6
Joel Beck <joel.beck@t-online.de>
2022-01-20 19:26:36 +0100

    added tests for eval_cqr()

---
+++ R/eval_cqr.R
@@ -0,0 +1,131 @@
+eval_by_one <- function(df_combined, summarise_by) {
+  df_combined |>
+    extract_validation_set() |>
+    scoringutils::eval_forecasts(summarise_by = c("method", summarise_by)) |>
+    dplyr::select(method:interval_score) |>
+    tidyr::pivot_wider(names_from = method, values_from = interval_score) |>
+    dplyr::mutate(relative_change = (cqr - original) / original) |>
+    dplyr::select(-c(cqr, original))
+}
+
+convert_row_types <- function(df, new_row) {
+  for (i in seq_along(df)) {
+    dtype <- class(df[[i]])
+    if (dtype == "character") {
+      new_row[[i]] <- as.character(new_row[[i]])
+    } else if (dtype == "numeric") {
+      new_row[[i]] <- as.numeric(new_row[[i]])
+    }
+  }
+  return(new_row)
+}
+
+add_row <- function(df, row) {
+  new_row <- as.list(row)
+
+  # convert to compatible data types for appending
+  df[nrow(df) + 1, ] <- convert_row_types(df, new_row)
+  return(df)
+}
+
+add_margins <- function(df, row_margins, col_margins) {
+  df |>
+    # exclude column of first category
+    add_row(c(NA, col_margins)) |>
+    # exclude new appended row
+    dplyr::mutate(margins = c(row_margins, NA))
+}
+
+geometric_mean <- function(x) {
+  prod(x)^(1 / length(x))
+}
+
+add_row_averages <- function(df) {
+  average_change <- df |>
+    # values are percentage changes => add one to get multiplicative changes
+    dplyr::mutate(dplyr::across(.cols = -1, .fns = ~ .x + 1)) |>
+    dplyr::rowwise() |>
+    # calculate geometric mean of multiplicative factors in each row and
+    # subtract one again to get percentage change
+    dplyr::summarise(
+      average_change = geometric_mean(dplyr::c_across(cols = -1)) - 1
+    ) |>
+    dplyr::pull(average_change)
+
+  df |> dplyr::mutate(average_change = average_change)
+}
+
+add_col_averages <- function(df) {
+  average_change <- df |>
+    # calculate geometric mean of multiplicative factors in each column
+    dplyr::mutate(dplyr::across(.cols = -1, .fns = ~ .x + 1)) |>
+    dplyr::summarise(dplyr::across(.cols = -1, .fns = geometric_mean) - 1)
+
+  df |> add_row(c(NA, as.numeric(average_change[1, ])))
+}
+
+round_output <- function(df, round_digits) {
+  if (!is.null(round_digits)) {
+    df <- df |>
+      dplyr::mutate(dplyr::across(
+        .cols = -1, .fns = ~ round(.x, round_digits)
+      ))
+  }
+
+  return(df)
+}
+
+eval_cqr <- function(df_combined, summarise_by, margins = FALSE,
+                     row_averages = FALSE, col_averages = FALSE,
+                     round_digits = 4) {
+  if ((margins && row_averages) || (margins && col_averages)) {
+    stop("Either margins or averages can be specified.")
+  }
+
+  result_long_format <- eval_by_one(df_combined, summarise_by)
+
+  # if only one category is specified
+  if (length(c(summarise_by)) == 1) {
+    return(
+      result_long_format |>
+        # output only sorted for single category
+        dplyr::arrange(relative_change) |>
+        # output is always rounded in last step to not impact intermediary
+        # calculations
+        round_output(round_digits)
+    )
+  }
+
+  # display horizon in increasing order for two-dimensional display
+  if ("horizon" %in% summarise_by) {
+    result_long_format <- result_long_format |> dplyr::arrange(horizon)
+  }
+
+  result_wide_format <- result_long_format |>
+    tidyr::pivot_wider(
+      names_from = .data[[summarise_by[2]]], values_from = relative_change
+    )
+
+  # either margins or table averages can be added
+  if (margins) {
+    row_margins <- eval_by_one(df_combined, summarise_by = summarise_by[1]) |>
+      dplyr::pull(relative_change)
+    col_margins <- eval_by_one(df_combined, summarise_by = summarise_by[2]) |>
+      dplyr::pull(relative_change)
+
+    return(
+      add_margins(result_wide_format, row_margins, col_margins) |>
+        round_output(round_digits)
+    )
+  }
+
+  if (row_averages) {
+    result_wide_format <- result_wide_format |> add_row_averages()
+  }
+
+  if (col_averages) {
+    result_wide_format <- result_wide_format |> add_col_averages()
+  }
+
+  return(result_wide_format |> round_output(round_digits))
+}
---
+++ tests/testthat/test-eval_cqr.R
@@ -0,0 +1,131 @@
+#   ____________________________________________________________________________
+#   Tests for UK Data                                                       ####
+
+df_combined <- readr::read_rds("test-data/uk_cqr.rds")
+
+### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+### Tests for single category                                               ####
+
+eval_models <- eval_cqr(df_combined, summarise_by = "model")
+eval_target_types <- eval_cqr(df_combined, summarise_by = "target_type")
+eval_horizons <- eval_cqr(df_combined, summarise_by = "horizon")
+eval_quantiles <- eval_cqr(df_combined, summarise_by = "quantile")
+
+test_that("works for single category", {
+  expect_equal(dim(eval_models), c(6, 2))
+  expect_equal(colnames(eval_models), c("model", "relative_change"))
+
+  expect_equal(dim(eval_target_types), c(2, 2))
+  expect_equal(colnames(eval_target_types), c("target_type", "relative_change"))
+
+  expect_equal(dim(eval_horizons), c(4, 2))
+  expect_equal(colnames(eval_horizons), c("horizon", "relative_change"))
+
+  expect_equal(dim(eval_quantiles), c(23, 2))
+  expect_equal(colnames(eval_quantiles), c("quantile", "relative_change"))
+})
+
+
+### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+### Tests for two categories                                                ####
+
+eval_models_horizons <- eval_cqr(
+  df_combined,
+  summarise_by = c("model", "horizon")
+)
+
+eval_models_target_types <- eval_cqr(
+  df_combined,
+  summarise_by = c("model", "target_type")
+)
+
+eval_horizons_target_types <- eval_cqr(
+  df_combined,
+  summarise_by = c("horizon", "target_type")
+)
+
+test_that("works for two categories", {
+  expect_equal(dim(eval_models_horizons), c(6, 5))
+  expect_equal(dim(eval_models_target_types), c(6, 3))
+  expect_equal(dim(eval_horizons_target_types), c(4, 3))
+})
+
+
+### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+### Tests for input margins                                                 ####
+
+margins_models_horizons <- eval_cqr(
+  df_combined,
+  summarise_by = c("model", "horizon"), margins = TRUE
+)
+
+row_margins <- margins_models_horizons$margins
+col_margins <- margins_models_horizons[nrow(margins_models_horizons), ]
+
+test_that("margins are added correctly", {
+  expect_equal(sum(row_margins, na.rm = TRUE), sum(eval_models$relative_change))
+  expect_equal(sum(as.numeric(col_margins), na.rm = TRUE), sum(eval_horizons$relative_change))
+})
+
+
+### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+### Tests for input averages                                                ####
+
+row_horizon_target_type <- eval_cqr(
+  df_combined,
+  summarise_by = c("horizon", "target_type"), row_averages = TRUE
+)
+
+col_horizon_target_type <- eval_cqr(
+  df_combined,
+  summarise_by = c("horizon", "target_type"), col_averages = TRUE
+)
+
+row_col_horizon_target_type <- eval_cqr(
+  df_combined,
+  summarise_by = c("horizon", "target_type"), row_averages = TRUE,
+  col_averages = TRUE
+)
+
+test_that("row and column averages work", {
+  expect_equal(dim(row_horizon_target_type), c(4, 4))
+  expect_equal(dim(col_horizon_target_type), c(5, 3))
+  expect_equal(dim(row_col_horizon_target_type), c(5, 4))
+})
+
+### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+### Tests for input round_digits                                            ####
+
+test_that("works for different numeric value of round_digits", {
+  eval_models_rounded <- eval_cqr(df_combined, summarise_by = "model", round_digits = 1)
+
+  expect_equal(
+    eval_models_rounded[1, 2], round(eval_models_rounded[1, 2], digits = 1)
+  )
+})
+
+test_that("NULL value of round_digits does not trigger error", {
+  expect_error(
+    eval_cqr(df_combined, summarise_by = "model", round_digits = NULL), NA
+  )
+})
+
+### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+### Tests for error messages                                                ####
+
+test_that("error messages are triggered as intended", {
+  expect_error(
+    eval_cqr(df_combined,
+      summarise_by = "model", margins = TRUE,
+      row_averages = TRUE
+    ),
+    "Either margins or averages can be specified."
+  )
+  expect_error(
+    eval_cqr(df_combined,
+      summarise_by = "model", margins = TRUE,
+      col_averages = TRUE
+    ),
+    "Either margins or averages can be specified."
+  )
+})

43b86a4bf1c14c55f113ee14ea5ad70234e45289
MatthiasHerp <63145900+MatthiasHerp@users.noreply.github.com>
2022-03-01 16:43:55 +0100

    updated files so that there is now a sequence and parallel option. qsa now has a body subfunction called by the parallel and sequence update_subset versions.

--- R/parallel_qsa.R
+++ R/parallel_qsa.R
@@ -188,38 +188,32 @@ optimize_spread_factor <- function(method, subset, penalty_weight, optim_method,
 }
 
 
-parallel_update_subset_qsa <- function(subset, method, cv_init_training, penalty_weight, optim_method, lower_bound_optim, upper_bound_optim, steps_optim) {
-  # must be placed on filtered data frame (i.e. lowest level, not in
-  # update_predictions()) such that fractional inputs can be correctly converted
-  cv_init_training <- validate_cv_init(df, cv_init_training)
-  
-
+body_update_subset_qsa <- function(subset, method, cv_init_training, penalty_weight, optim_method, lower_bound_optim, upper_bound_optim, steps_optim){
   # Moved the extraction of the dataframe into the if else condition as we use the data as it comes out of filter
-
   if (is.null(cv_init_training)) {
     # By default cv_init_training is equal to NULL and therefore equal to the complete data.
     # e.g. by default no split in training and validation set
-
-
+    
+    
     # Run optimization to get the optimal spread factor
     optimal_spread_factor <- optimize_spread_factor(method = method, subset = subset,
                                                     penalty_weight = penalty_weight, optim_method = optim_method, 
                                                     lower_bound_optim = lower_bound_optim, upper_bound_optim = upper_bound_optim, steps_optim = steps_optim,
                                                     par = NULL)
-
+    
     # function to apply optimal spread factor to data
     # We return a matrix containing the updates of the quantiles
     updates_matrix <- qsa(
       subset = subset, spread_factor_vec = optimal_spread_factor, method = method
     )
-
+    
     # copy of subset to fill with updates
     subset_updated <- subset
-
+    
     # extracting all quantiles that arnt the median
     quantiles_list <- stats::na.omit(unique(subset$quantile))
     quantiles_list_no_median <- quantiles_list[!quantiles_list == 0.50]
-
+    
     # updating the subset by exchanging the values in the dataframe with the ones from the update matrix
     # Note: which(quantiles_list_no_median == q) tells us the number of the quantile and thus of the update_matrix
     for (q in quantiles_list_no_median) {
@@ -233,18 +227,18 @@ parallel_update_subset_qsa <- function(subset, method, cv_init_training, penalty
   } else {
     # This Section runs the Time Series Cross validation.
     # 1. It runs the method on the training set and updates all values of the training set.
-
-
+    
+    
     # then extract the sorted list of target end dates and from there the training target end dates
     target_end_date_train <- sort(unique(subset$target_end_date))[0:cv_init_training]
     # reduce subset to the training period
     subset_train <- dplyr::filter(subset, .data$target_end_date %in% target_end_date_train)
-
+    
     # target end date for val
     target_end_date_val <- sort(unique(subset$target_end_date))[cv_init_training + 1]
     # subset for one step ahead prediction
     subset_val <- dplyr::filter(subset, .data$target_end_date == target_end_date_val)
-
+    
     # Run optimization to get the optimal spread factor
     optimal_spread_factor <- optimize_spread_factor(method = method, subset = subset_train, ###########Big typo: before we had only subset here so trained on full subset no CV!
                                                     penalty_weight = penalty_weight, optim_method = optim_method,
@@ -255,25 +249,25 @@ parallel_update_subset_qsa <- function(subset, method, cv_init_training, penalty
     updates_matrix <- qsa(subset = subset_train, spread_factor_vec = optimal_spread_factor, method = method) ###########Big typo: before we had only subset here so trained on full subset no CV!
     # one step ahead prediction adjustment
     val_row <- qsa(subset = subset_val, spread_factor_vec = optimal_spread_factor, method = method)
-
+    
     # appending the one step ahead prediciton to the update matrix
     updates_matrix <- rbind(updates_matrix, val_row)
-
+    
     # 2. The loop goes increases the training set by one observation each step
     #    and computes the next validation set
     #    prediction. This is done by rerunning cqr with the new observations set and extracting the margin.
     #    Then with the new margin the one horizon step ahead prediction is updated.
     for (training_length in (cv_init_training + 1):(length(unique(subset$target_end_date)) - 1)) {
-
+      
       # gets subsets of the subset
       target_end_date_train <- sort(unique(subset$target_end_date))[0:training_length]
       subset_train <- dplyr::filter(subset, .data$target_end_date %in% target_end_date_train)
-
+      
       target_end_date_val <- sort(unique(subset$target_end_date))[training_length + 1]
       subset_val <- dplyr::filter(subset, .data$target_end_date == target_end_date_val)
-
+      
       # for faster computation it starts optimization at the optimal spread factor of the last iteration
-
+      
       # Run optimization to get the optimal spread factor
       optimal_spread_factor <- optimize_spread_factor(method = method,subset = subset_train, ###########Big typo: before we had only subset here so trained on full subset no CV!
                                                       penalty_weight = penalty_weight, optim_method = optim_method,
@@ -282,37 +276,86 @@ parallel_update_subset_qsa <- function(subset, method, cv_init_training, penalty
       
       # For the iteration forward we only need the validation set prediction
       val_row <- qsa(subset = subset_val, spread_factor_vec = optimal_spread_factor, method = method)
-
+      
       updates_matrix <- rbind(updates_matrix, val_row)
     }
-
+    
     # copy of subset to fill with updates
     subset_updated <- subset
-
+    
     # extracting all quantiles that arnt the median
     quantiles_list <- stats::na.omit(unique(subset$quantile))
     quantiles_list_no_median <- quantiles_list[!quantiles_list == 0.50]
-
+    
     # updating the subset by exchanging the values in the dataframe with the ones from the update matrix
     # Note: which(quantiles_list_no_median == q) tells us the number of the quantile and thus of the update_matrix
     for (q in quantiles_list_no_median) {
       # updating the subset
       subset_updated <- subset_updated |>
         dplyr::mutate(prediction = replace(.data$prediction,
-          .data$quantile == q,
-          values = updates_matrix[, which(quantiles_list_no_median == q)]
+                                           .data$quantile == q,
+                                           values = updates_matrix[, which(quantiles_list_no_median == q)]
         ))
     }
   } ##########this closed bracket was at the end of the file before. therefore the no cv version returned no updated values!
-
+  
   # Make sure the updated subset is arranged by quantiles and then target end dates
   # That way we can pass its prediction column to the updated df directly
   subset_updated <- subset_updated |>
     dplyr::arrange(.data$quantile, .data$target_end_date)
-
+  
   quantiles_list <- stats::na.omit(unique(subset$quantile))
   quantiles_list_no_median <- quantiles_list[!quantiles_list == 0.50]
-
+  
   return(subset_updated)
 }
+
+
+parallel_update_subset_qsa <- function(subset, method, cv_init_training, penalty_weight, optim_method, lower_bound_optim, upper_bound_optim, steps_optim) {
+  # must be placed on filtered data frame (i.e. lowest level, not in
+  # update_predictions()) such that fractional inputs can be correctly converted
+  cv_init_training <- validate_cv_init(df, cv_init_training)
+  
+  subset_updated <- body_update_subset_qsa(subset, method, cv_init_training, penalty_weight, optim_method, lower_bound_optim, upper_bound_optim, steps_optim)
+  
+  return(subset_updated)
+}
+
+sequence_update_subset_qsa <- function(df, method, model, location, target_type, horizon, cv_init_training, penalty_weight, optim_method, lower_bound_optim, upper_bound_optim, steps_optim) {
+  # must be placed on filtered data frame (i.e. lowest level, not in
+  # update_predictions()) such that fractional inputs can be correctly converted
+  cv_init_training <- validate_cv_init(df, cv_init_training)
+  
+  m <- model
+  l <- location
+  t <- target_type
+  h <- horizon
+  
+  # gets subset of the data
+  subset <- dplyr::filter(df, model == m & location == l & target_type == t & horizon == h)
+  
+  subset_updated <- body_update_subset_qsa(subset, method, cv_init_training, penalty_weight, optim_method, lower_bound_optim, upper_bound_optim, steps_optim)
+  
+  # Again we first arrange the data to make we can pass the prediction column to the updated df directly
+  df_updated <- df |>
+    dplyr::arrange(.data$quantile, .data$target_end_date) |>
+    dplyr::mutate(prediction = replace(
+      .data$prediction,
+      .data$model == m & .data$location == l & .data$target_type == t & .data$horizon == h, # no quantile specified in quantile spread post processing
+      values = subset_updated$prediction
+    ))
+  
+  # set training length as attribute for plotting vertical line
+  attr(df_updated, "cv_init_training") <- cv_init_training
+  
+  return(df_updated)
+}
 #TODO: write helper function that computes the applied optimal spread parameters of a qsa approach as well as the actual absolute adjustment. this is a nice way to compare the different adjustments different methods do
+
+
+
+
+
+
+
+

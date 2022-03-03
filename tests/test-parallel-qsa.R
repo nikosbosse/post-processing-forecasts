966d00b96a82b9d2545144cb673545bd73a0100e
MatthiasHerp <63145900+MatthiasHerp@users.noreply.github.com>
2022-03-03 11:04:34 +0100

    Wrote Test that checks if QSA parallel can handle empty combinations of model, location and target type. Solved that it now can by adding an if condition checking for non zero rows within update predictions where all combinations are prepared. Further pushed testing dev files into archiv that are not required, unless issue still persists.

--- tests/test-parallel-qsa.R
+++ tests/test-parallel-qsa.R
@@ -1,7 +1,3 @@
-
-
-#   ____________________________________________________________________________
-#   Test that parallel and sequence processing of qsa give the same result  ####
 devtools::load_all(".")
 library(foreach)
 library(doParallel)
@@ -12,6 +8,8 @@ registerDoParallel(cores = 2)
 # library(tictoc)
 
 
+#   ____________________________________________________________________________
+#   Test that parallel and sequence processing of qsa give the same result  ####
 df <- read.csv(here::here("data_modified", "uk_data_incidences.csv"))
 m <- "epiforecasts-EpiExpert"
 l <- "GB"
@@ -87,4 +85,46 @@ test_that("sequence and parallel computation give back identical values.", {
       df_updated_parallel$qsa_uniform[, "horizon"]
     ),
   ]))
+})
+
+
+#   ____________________________________________________________________________
+#   Test that parallel processing can handle combinations in df with no obs ####
+
+
+# Examples for combinations with no observations: | quantile observations = 0 | model = anonymous_Magpie | location = GB | target_type = Deaths | horizon = 1
+df <- read.csv(here::here("data_modified", "uk_data_incidences.csv"))
+m <- c("epiforecasts-EpiExpert","anonymous_Goldfinch")
+l <- "GB"
+t <- c("Cases","Deaths")
+h <- c(1, 2)
+
+df_subset <- dplyr::filter(df, model %in% m & location %in% l &
+                             target_type %in% t & horizon %in% h)
+
+# Tests wether subset contains all combiantions we expect
+unique(df_subset$model) == m
+unique(df_subset$location) == l
+unique(df_subset$target_type) == t
+unique(df_subset$horizon) == h
+nrow(dplyr::filter(df, model %in% "anonymous_Goldfinch" & location %in% "GB" & target_type %in% "Deaths" & horizon %in% 1)) == 0
+
+
+
+# Run QSA parallel
+# tic()
+df_updated_parallel <- update_predictions(df_subset,
+                                          methods = "qsa_uniform",
+                                          # cv_init_training = 11,
+                                          optim_method = "L-BFGS-B",
+                                          lower_bound_optim = 0,
+                                          upper_bound_optim = 5,
+                                          return_list = TRUE,
+                                          parallel = TRUE,
+                                          verbose = TRUE
+)
+# toc()
+# 272.797 sec elapsed
+test_that("parallel computation of qsa gives back the right number of rows, e.g. as many as in original data even if some combinations of model, location, target_type and horizon dont have observations", {
+  expect_equal(TRUE, nrow(df_updated_parallel$qsa_uniform) == nrow(df_updated_parallel$original))
 })
